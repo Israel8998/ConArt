66c15894c3b1492e3692026e5ba066e156122f9c
Israel8998 <israel_pazmi@hotmail.com>
2022-07-08 14:58:25 -0500

    Creación todas las pantallas vacías que se van a utilizar

---
+++ app/src/main/java/com/example/conart/VerDatos.kt
@@ -0,0 +1,11 @@
+package com.example.conart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerDatos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ver_datos)
+    }
+}
