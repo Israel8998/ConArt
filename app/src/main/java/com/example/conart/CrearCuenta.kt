54e50f271717d34dd9a959e02f36a87ef8a79c1d
Israel8998 <israel_pazmi@hotmail.com>
2022-07-15 21:42:40 -0500

    creación e ingreso de usuarios

--- app/src/main/java/com/example/conart/CrearCuenta.kt
+++ app/src/main/java/com/example/conart/CrearCuenta.kt
@@ -1,11 +1,66 @@
 package com.example.conart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_crear_cuenta.*
 
 class CrearCuenta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_cuenta)
+
+
+        //Analytuc Event
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
+
+        //Setup
+        setup()
+    }
+
+    private fun setup() {
+        title = "Registro"
+
+        btnRegistrar.setOnClickListener {
+            if (txtCorreoUsuario.text.isNotEmpty() && txtPassword.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(txtCorreoUsuario.text.toString(),
+                    txtPassword.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showSatisfaction()
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+
+        btnIniciarSesion.setOnClickListener {
+            val iniciarSesion = Intent(this, IniciarSesion::class.java)
+            startActivity(iniciarSesion)
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showSatisfaction() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Registro exitoso")
+        builder.setMessage("Usuario registrado con éxito")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/conart/IniciarSesion.kt
+++ app/src/main/java/com/example/conart/IniciarSesion.kt
@@ -1,11 +1,54 @@
 package com.example.conart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_crear_cuenta.*
+import kotlinx.android.synthetic.main.activity_iniciar_sesion.*
 
 class IniciarSesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion)
+
+        setup()
+    }
+    private fun setup() {
+        title = "Iniciar sesión"
+
+        aRegistrar.setOnClickListener {
+            val intent = Intent(this, CrearCuenta::class.java)
+            startActivity(intent)
+        }
+
+        btnIngresar.setOnClickListener {
+            if (txtCorreo.text.isNotEmpty() &&
+                txtContraseña.text.isNotEmpty()) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(txtCorreo.text.toString(),
+                    txtContraseña.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        showIngresarDatos()
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Correo o contraseña incorrectos")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showIngresarDatos() {
+        val InicioSesion = Intent(this, IngresoDatos::class.java)
+        startActivity(InicioSesion)
     }
 }
